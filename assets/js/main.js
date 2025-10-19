f52b1cb6d687d132dc36ab155404f2e11f3434e5
FasyaRepo <96086522+FasyaRepo@users.noreply.github.com>
2025-10-20 06:18:38 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -164,10 +164,8 @@
    // Ganti isi span sesuai parameter
   if (nama) {
     spanNama.textContent = decodeURIComponent(nama);
-    document.title = `Undangan untuk ${decodedNama} | Fasya & Mira`;
   } else {
     spanNama.textContent = "Di Tempat";
-    document.title = "Undangan Pernikahan | Fasya & Mira";
   }
 
   window.addEventListener('load', () => {
